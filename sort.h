986429de7aa087454fc7b902331b75fbaa62b18c
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-14 03:01:58 +0000

    Task1

--- sort.h
+++ sort.h
@@ -14,7 +14,7 @@ typedef struct listint_s
 const int n;
 struct listint_s *prev;
 struct listint_s *next;
-} listint_t;
+} typedef struct listint_s listint_t;
 
 /** Print functions **/
 void print_array(const int *array, size_t size);
