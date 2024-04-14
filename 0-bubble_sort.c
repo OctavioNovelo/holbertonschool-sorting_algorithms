7eccda57c1f8118d7a92c296f58edda478f322dc
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-14 01:25:58 +0000

    Task0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,25 @@
 #include "sort.h"
-
 /**
  *
  */
-
-void bubble_sort(int *array, size_t size);
-
+void bubble_sort(int *array, size_t size)
+{
 int temp;
 int i, j;
 
 for (i = 0; i<size-1; ++i)
 {
   for (j = 0; j<size-1; ++j)
-   {
+    {
      if (array[j]>array[j+1])
        {
 	 temp = array[j];
 	 array[j] = array[j+1];
 	 array[j+1] = temp;
 	 print_array(array, size)
-	}
+	   }
    }
-}
-return (0);
+ }
 
+ return (0);
+}
