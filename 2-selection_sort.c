8b6ff1698926cb3b8ba5fc90e4b6915a056ab553
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-14 04:02:29 +0000

    Task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,33 @@
 #include "sort.h"
-
 /**
  * selection_sort - sorts an array of integers in ascending order using
  * the Selection sort algorithm
  * @array: Array of integers
  * @size: Size of array
  */
-
 void selection_sort(int *array, size_t size)
 {
-	int temp;
-	size_t i, j;
-	int val;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		val = array[i];
-	}
-	for (j = 1; j < size - 1; j++)
-	{
-		if (array[j] < val)
-		{
-			temp = array[j];
-		}
-		array[i] = temp;
-		print_array(array, size);
-	}
+int temp;
+size_t i, j, val;
+for (i = 0; i < size - 1; i++)
+{
+val = i;
+for (j = 1; j < size; j++)
+{
+if (array[j] < array[val])
+{
+val = j;
+}
+array[i] = temp;
+print_array(array, size);
+}
+if (val != i)
+{
+temp = array[i];
+array[i] = array[val];
+array[val] = tmp;
+print_array(array, size);
+}
+}
 }
 
