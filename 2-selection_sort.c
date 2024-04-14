714cd76685909026335627ee3cbc6acffd019e77
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-14 04:09:15 +0000

    Task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,40 @@
 #include "sort.h"
 /**
- * selection_sort - sorts an array of integers in ascending order using
- * the Selection sort algorithm
- * @array: Array of integers
- * @size: Size of array
+ * selection_sort - Calls function
+ * @array: Array to be sorted
+ * @size: Size of array given
+ * Return: 0
  */
 void selection_sort(int *array, size_t size)
 {
-int temp;
-size_t i, j, val;
+unsigned int i, j, val;
+if (!array)
+return;
 for (i = 0; i < size - 1; i++)
 {
 val = i;
 for (j = i + 1; j < size; j++)
-{
 if (array[j] < array[val])
-{
 val = j;
-}
-array[i] = temp;
-print_array(array, size);
-}
-if (val != i)
+if (i != val)
 {
-temp = array[i];
-array[i] = array[val];
-array[val] = temp;
+swap(&array[val], &array[i]);
 print_array(array, size);
 }
 }
+}
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
+int tmp;
+tmp = *b;
+*b = *a;
+*a = tmp;
 }
 
