f7f6de164d399b2dde6b5bd1f1a817d0e77a8891
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-14 01:38:54 +0000

    Task0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,11 +8,11 @@ void bubble_sort(int *array, size_t size)
 {
 int temp;
 size_t i, j;
-for (i = 0; i < size - 1; ++ i)
+for (i = 0; i < size - 1; + + i)
 {
-for (j = 0; j < size - 1; ++ j)
+for (j = 0; j < size - 1; + + j)
 {
-if (array[j]>array[j + 1])
+if (array[j] > array[j + 1])
 {
 temp = array[j];
 array[j] = array[j + 1];
