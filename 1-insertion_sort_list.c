7ad29c1ccdf3afc68375f55a5641de4dfb2de9c8
Dierak <diego.b.c.rodriguez@gmail.com>
2024-04-14 02:54:17 +0000

    Task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ *
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	int i;
+	int val;
+	int pos;
+	size_t lista = sizeof(listint_t);
+	int tmp;
+
+	for ( i = 0, i < lista, i++)
+	{
+		val = next;
+		pos = next;
+
+		while (val > prev || val < prev) 
+		{
+			if (val < prev)
+			{
+			tmp = prev;
+			prev = val;
+			pos = tmp;
+			i++;
+			print_list((const listint_t *)*list);
+			}
+
+			if (val > prev)
+			{
+				tmp = prev;
+				prev = val;
+				pos = tmp;
+			print_list((const listint_t*)*list);
+			}
+		}
+	}
+}
+
+
